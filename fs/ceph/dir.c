88b426040059182325fc2ebca193cf683dcd43eb
Amon Ott <ao@rsbac.org>
2024-09-03 10:07:49 +0200

    With "Hide filesystem objects", hide in all places where CAP FD hiding does. CONFIG_RSBAC_FSOBJ_HIDE hides based on SEARCH right, this means extra SEARCH requests for all covered items.

--- fs/ceph/dir.c
+++ fs/ceph/dir.c
@@ -200,6 +200,12 @@ static int __dcache_readdir(struct file *file,  struct dir_context *ctx,
 	u64 idx = 0;
 	int err = 0;
 
+#if defined(CONFIG_RSBAC_FSOBJ_HIDE)
+	enum  rsbac_target_t          rsbac_target;
+	union rsbac_target_id_t       rsbac_target_id;
+	union rsbac_attribute_value_t rsbac_attribute_value;
+#endif
+
 	doutc(cl, "%p %llx.%llx v%u at %llx\n", dir, ceph_vinop(dir),
 	      (unsigned)shared_gen, ctx->pos);
 
@@ -269,8 +275,36 @@ static int __dcache_readdir(struct file *file,  struct dir_context *ctx,
 
 		if (emit_dentry) {
 
+#if defined(CONFIG_RSBAC_CAP_FD_HIDE) || defined(CONFIG_RSBAC_FSOBJ_HIDE)
+			bool do_hide = false;
+
 #if defined(CONFIG_RSBAC_CAP_FD_HIDE)
-			if(!rsbac_cap_hide_fd(d_inode(dentry))) {
+			if(rsbac_cap_hide_fd(d_inode(dentry)))
+				do_hide = true;
+#endif
+#if defined(CONFIG_RSBAC_FSOBJ_HIDE)
+			rsbac_target = T_FILE;
+			if (S_ISDIR(d_inode(dentry)->i_mode))
+				rsbac_target = T_DIR;
+			else if (S_ISFIFO(d_inode(dentry)->i_mode))
+				rsbac_target = T_FIFO;
+			else if (S_ISLNK(d_inode(dentry)->i_mode))
+				rsbac_target = T_SYMLINK;
+			else if (S_ISSOCK(d_inode(dentry)->i_mode))
+				rsbac_target = T_UNIXSOCK;
+			rsbac_target_id.file.device = d_inode(dentry)->i_sb->s_dev;
+			rsbac_target_id.file.inode  = d_inode(dentry)->i_ino;
+			rsbac_target_id.file.dentry_p = dentry;
+			rsbac_attribute_value.dummy = 0;
+			if (!rsbac_adf_request(R_SEARCH,
+						task_pid(current),
+						rsbac_target,
+						rsbac_target_id,
+						A_none,
+						rsbac_attribute_value))
+				do_hide = true;
+#endif
+			if (!do_hide) {
 #endif
 
 			doutc(cl, " %llx dentry %p %pd %p\n", di->offset,
@@ -285,7 +319,7 @@ static int __dcache_readdir(struct file *file,  struct dir_context *ctx,
 			}
 			ctx->pos++;
 
-#if defined(CONFIG_RSBAC_CAP_FD_HIDE)
+#if defined(CONFIG_RSBAC_CAP_FD_HIDE) || defined(CONFIG_RSBAC_FSOBJ_HIDE)
 			}
 #endif
 
